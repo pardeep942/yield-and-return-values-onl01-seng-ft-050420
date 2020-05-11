68bbd9442f26bcc2611376b165d3682b89e0fe77
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 06:00:22 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -11,3 +11,12 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
